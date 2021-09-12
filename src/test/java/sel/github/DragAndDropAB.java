b78d7ff321607e48ce65652aea0628a05b6d6ea7
ECLIPSE170790 <eclipse170790@mail.ru>
2021-09-12 14:00:38 +0600

    Created DragAndDropAB

---
+++ src/test/java/sel/github/DragAndDropAB.java
@@ -0,0 +1,30 @@
+package sel.github;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DragAndDropAB {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void shouldFindSelenideGitHub(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        //sleep(5000);
+        Selenide.closeWindow();
+        Selenide.closeWebDriver();
+
+    }
+}
